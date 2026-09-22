c9cb00f85467e6a94508aae004f326f9e92a6f2a
Lucero Galeano <lucero.galeano@correo.tdea.edu.co>
2026-09-22 05:15:16 -0500

    Proyecto base

---
+++ src/main/java/com/odontologia/Vitaldent_PPI/VitaldentPpiApplication.java
@@ -0,0 +1,13 @@
+package com.odontologia.Vitaldent_PPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitaldentPpiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitaldentPpiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/odontologia/Vitaldent_PPI/VitaldentPpiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odontologia.Vitaldent_PPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitaldentPpiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
